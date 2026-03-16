cdec717c7ea2b04f373f50b9a84867ef1b3e0fcd
Denwien <74426537+ffdfd2424325@users.noreply.github.com>
2026-03-16 22:25:12 +0300

    Fix GitHub Pages base path

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/garant-flow-hub">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/catalog" element={<Catalog />} />
